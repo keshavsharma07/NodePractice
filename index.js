09d36fa9bc9e67b3776dd641fa3d3744c6be86cb
Keshav Sharma <keshavgalav07@gmail.com>
2020-10-30 12:50:26 +0530

    Read Write File

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const fs=require("fs");
 fs.writeFileSync("read.txt","Welcome boo");
 
 fs.writeFileSync("read.txt","Keshav,Welcome");
+
+//reading data of another file
+
+const buf_data=fs.readFileSync("read.txt");
+// console.log(buf_data);
+ori_data=buf_data.toString();
+console.log(ori_data);
