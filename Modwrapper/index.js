deae9f488da677013e99338a335d28fe6f65ac3a
Keshav Sharma <keshavgalav07@gmail.com>
2020-11-08 09:00:55 +0530

    Wrapper function

---
+++ Modwrapper/index.js
@@ -0,0 +1,5 @@
+(function ( exports,require,module,__filename,__dirname){
+
+    const name ="Keshav";
+    console.log(name);
+})
--- npmMod/index.js
+++ npmMod/index.js
@@ -4,5 +4,5 @@ const validator=require("validator");
 console.log(chalk.blue.underline("Hello World"));
 
 
-const res =validator.isEmail("keshav@gmail.com");
+const res =validator.isEmail("keshav@gma.com");
 console.log(res ? chalk.green.inverse(res): chalk.red.inverse(res));
